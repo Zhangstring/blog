cfc0d6d941f70c5e7a049802455fcfe73d26f989
zxj <925877520@qq.com>
2019-11-25 15:30:09 +0800

    docs init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'zxj blog',
+  description: 'my blog',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: 'webpack', link: '/webpack/' },
+      { text: 'vue', link: '/vue/' },
+      { text: 'react', link: '/react/' }
+    ]
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,5 @@
+---
+home: true
+actionText: 查看详情
+actionLink: /webpack/
+---
