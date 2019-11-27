bf34c031b86a57a6ecd9b58b2d35c1f5e62b6f1c
zxj <925877520@qq.com>
2019-11-27 10:26:30 +0800

    chore: remove git dist file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# blog
+# Blog
+
+Blog are available at https://blog.zhangxujun.cn/ 
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,31 @@
+const fs = require('fs')
+const path = require('path')
 module.exports = {
   title: 'zxj blog',
   description: 'my blog',
   themeConfig: {
     nav: [
-      { text: '首页', link: '/' },
       { text: 'webpack', link: '/webpack/' },
       { text: 'vue', link: '/vue/' },
       { text: 'react', link: '/react/' }
-    ]
+    ],
+    sidebar: {
+      '/webpack/': getSideBarChildren('webpack')
+    }
   }
+}
+
+function getSideBarChildren(title) {
+  let sideBarPath = path.resolve(`./docs/${title}`)
+  let children = fs.readdirSync(sideBarPath).map(fileName => {
+    fileName = fileName.split('.')[0]
+    return fileName === 'README' ? '' : fileName
+  })
+  return [
+    {
+      title: title,
+      collapsable: false,
+      children: children
+    }
+  ]
 }
